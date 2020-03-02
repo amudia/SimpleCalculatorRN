fe4079bf2069487afc5d79a26c087bec14c87850
amudia <amudia.ktaruna@gmail.com>
2020-03-03 00:22:16 +0700

    fixed

--- App.js
+++ App.js
@@ -1,114 +1,342 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+import React, {Component} from 'react';
+import {Text, View, StyleSheet, TextInput, CheckBox} from 'react-native';
+import {Button} from 'react-native-paper';
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: 20,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: 'purple',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  wrapcontent: {
+    margin: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  body: {
-    backgroundColor: Colors.white,
+  wrapinput: {width: 250},
+  wrapcheck: {marginLeft: 20},
+  textinput: {
+    fontSize: 30,
+    textAlign: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  wrapbtn: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  btn: {margin: 5},
+  wraptext: {
+    marginHorizontal: 20,
+    marginVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+  text1: {fontSize: 20},
+  text2: {fontSize: 20, fontWeight: 'bold'},
 });
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input1: null,
+      input2: null,
+      input3: null,
+      checked1: false,
+      checked2: false,
+      checked3: false,
+      result: '',
+      error: null,
+    };
+  }
+
+  add = () => {
+    if (this.state.checked1 && this.state.checked2 && this.state.checked3) {
+      if (this.state.input1 && this.state.input2 && this.state.input3) {
+        this.setState({
+          result: this.state.input1 + this.state.input2 + this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked2) {
+      if (this.state.input1 && this.state.input2) {
+        this.setState({
+          result: this.state.input1 + this.state.input2,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked3) {
+      if (this.state.input1 && this.state.input3) {
+        this.setState({
+          result: this.state.input1 + this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked2 && this.state.checked3) {
+      if (this.state.input2 && this.state.input3) {
+        this.setState({
+          result: this.state.input2 + this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else {
+      this.setState({
+        result: 'Please check 2 or more checklist!',
+      });
+    }
+  };
+
+  min = () => {
+    if (this.state.checked1 && this.state.checked2 && this.state.checked3) {
+      if (this.state.input1 && this.state.input2 && this.state.input3) {
+        this.setState({
+          result: this.state.input1 - this.state.input2 - this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked2) {
+      if (this.state.input1 && this.state.input2) {
+        this.setState({
+          result: this.state.input1 - this.state.input2,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked3) {
+      if (this.state.input1 && this.state.input3) {
+        this.setState({
+          result: this.state.input1 - this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked2 && this.state.checked3) {
+      if (this.state.input2 && this.state.input3) {
+        this.setState({
+          result: this.state.input2 - this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else {
+      this.setState({
+        result: 'Please check 2 or more checklist!',
+      });
+    }
+  };
+
+  multiply = () => {
+    if (this.state.checked1 && this.state.checked2 && this.state.checked3) {
+      if (this.state.input1 && this.state.input2 && this.state.input3) {
+        this.setState({
+          result: this.state.input1 * this.state.input2 * this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked2) {
+      if (this.state.input1 && this.state.input2) {
+        this.setState({
+          result: this.state.input1 * this.state.input2,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked3) {
+      if (this.state.input1 && this.state.input3) {
+        this.setState({
+          result: this.state.input1 * this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked2 && this.state.checked3) {
+      if (this.state.input2 && this.state.input3) {
+        this.setState({
+          result: this.state.input2 * this.state.input3,
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else {
+      this.setState({
+        result: 'Please check 2 or more checklist!',
+      });
+    }
+  };
+
+  divide = () => {
+    if (this.state.checked1 && this.state.checked2 && this.state.checked3) {
+      if (this.state.input1 && this.state.input2 && this.state.input3) {
+        this.setState({
+          result: parseFloat(
+            this.state.input1 / this.state.input2 / this.state.input3,
+          ).toFixed(8),
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked2) {
+      if (this.state.input1 && this.state.input2) {
+        this.setState({
+          result: parseFloat(this.state.input1 / this.state.input2).toFixed(8),
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked1 && this.state.checked3) {
+      if (this.state.input1 && this.state.input3) {
+        this.setState({
+          result: parseFloat(this.state.input1 / this.state.input3).toFixed(8),
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else if (this.state.checked2 && this.state.checked3) {
+      if (this.state.input2 && this.state.input3) {
+        this.setState({
+          result: parseFloat(this.state.input2 / this.state.input3).toFixed(8),
+        });
+      } else {
+        this.setState({
+          result: 'Please check 2 or more checklist!',
+        });
+      }
+    } else {
+      this.setState({
+        result: 'Please check 2 or more checklist!',
+      });
+    }
+  };
+
+  checkBoxtTest = () => {
+    this.setState(prevState => ({checked1: !prevState.checked1}));
+  };
+  checkBoxtTest2 = () => {
+    this.setState(prevState => ({checked2: !prevState.checked2}));
+  };
+  checkBoxtTest3 = () => {
+    this.setState(prevState => ({checked3: !prevState.checked3}));
+  };
+
+  render() {
+    return (
+      <View>
+        <View>
+          <Text style={styles.title}> SIMPLE CALCULATOR </Text>
+        </View>
+        <View style={styles.wrapcontent}>
+          <View style={styles.wrapinput}>
+            <TextInput
+              keyboardType="numeric"
+              style={styles.textinput}
+              name="num1"
+              onChangeText={input => this.setState({input1: parseInt(input)})}
+            />
+          </View>
+          <View style={styles.wrapcheck}>
+            <CheckBox
+              color="#7b38c2"
+              value={this.state.checked1}
+              onChange={() => this.checkBoxtTest()}
+            />
+          </View>
+        </View>
+        <View style={styles.wrapcontent}>
+          <View style={styles.wrapinput}>
+            <TextInput
+              keyboardType="numeric"
+              style={styles.textinput}
+              name="num1"
+              onChangeText={input => this.setState({input2: parseInt(input)})}
+            />
+          </View>
+          <View style={styles.wrapcheck}>
+            <CheckBox
+              color="#7b38c2"
+              value={this.state.checked2}
+              onChange={() => this.checkBoxtTest2()}
+            />
+          </View>
+        </View>
+        <View style={styles.wrapcontent}>
+          <View style={styles.wrapinput}>
+            <TextInput
+              keyboardType="numeric"
+              style={styles.textinput}
+              name="num1"
+              onChangeText={input => this.setState({input3: parseInt(input)})}
+            />
+          </View>
+          <View style={styles.wrapcheck}>
+            <CheckBox
+              color="#7b38c2"
+              value={this.state.checked3}
+              onChange={() => this.checkBoxtTest3()}
+            />
+          </View>
+        </View>
+        <View style={styles.wrapbtn}>
+          <Button mode="outlined" onPress={this.add} style={styles.btn}>
+            +
+          </Button>
+          <Button mode="outlined" onPress={this.min} style={styles.btn}>
+            -
+          </Button>
+          <Button mode="outlined" onPress={this.multiply} style={styles.btn}>
+            x
+          </Button>
+          <Button mode="outlined" onPress={this.divide} style={styles.btn}>
+            /
+          </Button>
+        </View>
+        <View style={styles.wraptext}>
+          <Text style={styles.text1}>Hasil: </Text>
+          <Text style={styles.text2}>{this.state.result}</Text>
+        </View>
+      </View>
+    );
+  }
+}
